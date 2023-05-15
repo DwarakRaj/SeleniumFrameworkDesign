db71987a250e051346bfcfe66d1cf706973bdd2c
DwarakRaj <sathya_dwarak@yahoo.co.in>
2023-05-15 15:56:32 +0530

    Added comments

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -2,7 +2,8 @@ package cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-//cucumber->  TestNG, 
+//cucumber->  TestNG,Runner file
+
 
 @CucumberOptions(features="src/test/java/cucumber",glue="rahulshettyacademy.stepDefinitions",
 monochrome=true, tags = "@Regression", plugin= {"html:target/cucumber.html"})
